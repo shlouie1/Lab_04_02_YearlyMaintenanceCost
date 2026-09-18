5658aad3200dc64d5d9caf325c93a58f32bd02f8
shlouie1 <carterwiesnerofficial@gmail.com>
2026-09-18 13:41:31 -0400

    Completed task 2

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+public class Main {
+    public void main() {
+        int springMaitenanceCost = 450;
+        int summerMaitenanceCost = 550;
+        int fallMaitenanceCost = 230;
+        int winterMaitenanceCost = 4120;
+        int totalCost = 0;
+
+        totalCost = springMaitenanceCost + summerMaitenanceCost + fallMaitenanceCost + winterMaitenanceCost;
+
+        System.out.println("The cost of spring maitenance was $" + springMaitenanceCost);
+        System.out.println("The cost of summer maitenance was $" + summerMaitenanceCost);
+        System.out.println("The cost of fall maitenance was $" + fallMaitenanceCost);
+        System.out.println("The cost of winter maitenance was $" + winterMaitenanceCost);
+        System.out.println("The total cost of maitenance this year was $" + totalCost);
+    }
+}
