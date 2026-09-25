38bc64c039a1bded6a4d5b3d73c24d8d4c18cc2c
shlouie1 <carterwiesnerofficial@gmail.com>
2026-09-25 13:34:27 -0400

    added comments...

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,17 @@
 public class Main {
     public void main() {
+
+        //intialized variables of prices that i decided on
         int springMaitenanceCost = 450;
         int summerMaitenanceCost = 550;
         int fallMaitenanceCost = 230;
         int winterMaitenanceCost = 4120;
         int totalCost = 0;
 
+        //sum operation to find total price
         totalCost = springMaitenanceCost + summerMaitenanceCost + fallMaitenanceCost + winterMaitenanceCost;
 
+        //printed out costs of each season and final cost
         System.out.println("The cost of spring maitenance was $" + springMaitenanceCost);
         System.out.println("The cost of summer maitenance was $" + summerMaitenanceCost);
         System.out.println("The cost of fall maitenance was $" + fallMaitenanceCost);
